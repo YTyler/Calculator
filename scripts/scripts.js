2867cc1993b63b26897c98f4050ea28e5f4eb680
Student <example@example.com>
2019-10-21 10:25:58 -0700

    adds first iteration of calculator webapp

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+//Number Prompts
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+//Addition
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+alert(add(number1, number2));
+
+//Subtraction
+var subtraction = function(number1, number2){
+  return number1 - number2;
+}
+alert(subtraction(number1, number2))
+
+//Division
+var division = function(number1, number2){
+  return number1 / number2;
+}
+alert(division(number1, number2))
+//Multiplication
+var multiplication = function (number1, number2){
+  return number1 * number2;
+}
+alert(multiplication (number1, number2))  
