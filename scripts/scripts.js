426fe5700f418c2758b592d2ecf21807038d9950
Student <example@example.com>
2019-10-24 16:54:51 -0700

    branch to explore print error

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,47 +12,58 @@ var divide= function(number1, number2){
   };
 var errorMessage = "Can't divide by zero!";
 $(document).ready(function() {
-  // $("#calculatorForm").submit(function(event) {
-  //   number1 = parseInt($("input#number1").val());
-  //   number2 = parseInt($("input#number2").val());
-  //   result = parseInt(add(number1, number2));
-  //   console.log(number1);
-  //   console.log(number2);
-  //   console.log(result);
-  //   console.log("Type of: ", typeof(result));
-  //   event.preventDefault();
-  $('button#add').click(function() {
+  $("#calculatorForm").submit(function(event) {
     var number1 = parseInt($("input#number1").val());
     var number2 = parseInt($("input#number2").val());
-    var result = parseInt(add(number1, number2));
-    $("#additionResult").text(result);
-    $(".results").slideDown();
-    event.preventDefault();
-  });
-  $('button#subtract').click(function() {
-    var number1 = parseInt($("input#number1").val());
-    var number2 = parseInt($("input#number2").val());
-    var result = parseInt(subtract(number1, number2));
-    $("#subtractionResult").text(result);
-    $(".results").slideDown();
-    event.preventDefault();
-  });
-  $('button#multiply').click(function() {
-    var number1 = parseInt($("input#number1").val());
-    var number2 = parseInt($("input#number2").val());
-    var result = parseInt(multiply(number1, number2));
-    $("#multiplicationResult").text(result);
-    $(".results").slideDown();
-    event.preventDefault();
-  });
-  $('button#divide').click(function() {
-    var number1 = parseInt($("input#number1").val());
-    var number2 = parseInt($("input#number2").val());
-    if (number2 === 0) {
-      alert("Glitch in the matrix!")
-      event.preventDefault();
-      return errorMessage;
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "+") {
+      result = add(number1, number2);
+    } else if (operator === "-")  {
+      result = subtract(number1, number2);
+    } else if (operator === "*")  {
+      result = multiply(number1, number2);
+    } else if (operator === "/")  {
+      result = divide(number1, number2);
+    } else {
+      return ("We're Screwed.");
     }
+    $('#result').text(result);
+    $(".results").slideDown();
+    event.preventDefault();
+
+  // $('button#add').click(function() {
+  //   var number1 = parseInt($("input#number1").val());
+  //   var number2 = parseInt($("input#number2").val());
+  //   var result = parseInt(add(number1, number2));
+  //   $("#additionResult").text(result);
+  //   $(".results").slideDown();
+  //   event.preventDefault();
+  // });
+  // $('button#subtract').click(function() {
+  //   var number1 = parseInt($("input#number1").val());
+  //   var number2 = parseInt($("input#number2").val());
+  //   var result = parseInt(subtract(number1, number2));
+  //   $("#subtractionResult").text(result);
+  //   $(".results").slideDown();
+  //   event.preventDefault();
+  // });
+  // $('button#multiply').click(function() {
+  //   var number1 = parseInt($("input#number1").val());
+  //   var number2 = parseInt($("input#number2").val());
+  //   var result = parseInt(multiply(number1, number2));
+  //   $("#multiplicationResult").text(result);
+  //   $(".results").slideDown();
+  //   event.preventDefault();
+  // });
+  // $('button#divide').click(function() {
+  //   var number1 = parseInt($("input#number1").val());
+  //   var number2 = parseInt($("input#number2").val());
+  //   if (number2 === 0) {
+  //     alert("Glitch in the matrix!")
+  //     event.preventDefault();
+  //     return errorMessage;
+  //   }
     var result = parseFloat(divide(number1, number2)).toFixed(2);
     $("#divisionResult").text(result);
     $(".results").slideDown();
